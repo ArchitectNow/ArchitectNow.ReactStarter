ff74b66a45b3d57fba32a61b04bf5a4a108ae79c
Kevin Grossnicklaus <kvgros@architectnow.net>
2017-12-11 08:20:00 -0600

    Initial commit

---
+++ src/src/app/App.test.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import * as enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+it('is true', () => {
+    const contents = enzyme.shallow(<App />);
+
+    expect(contents.find('.App-title').text()).toEqual('Hello Daniel!');
+});
